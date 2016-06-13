92c416863ca35fdf30e0e7ca701f22c7e2e6b04e
山内英夫 <ac30cch@gmail.com>
2016-06-13 17:36:08 +0900

    text count

--- js/text/textcount.js
+++ js/text/textcount.js
@@ -1,18 +1,30 @@
-module.exports = {
-	textCount : function(target,countNum) {
-		var count = countNum;
-		$(target).each(function () {
-			var thisText = $(this).text().replace(/\s+/g,''),
-				textLength = thisText.length;
-			if (textLength > count) {
-				var showText = thisText.substring(0, count),
-					hideText = thisText.substring(count, textLength),
-					insertText = showText;
-				insertText += '<span class="hide">' + hideText + '</span>';
-				insertText += '<span class="omit">…</span>';
-				$(this).html(insertText);
+import $ from 'jquery'
+
+export default function textCount(target,lineNum) {
+	var $target = target;
+
+	var checkLh  = $target.css('line-height'),
+		checkHeight = parseInt(checkLh, 10),
+		constHeight = checkHeight*lineNum;
+
+	$target.each(function(){
+		var targetHeight = $(this).innerHeight(),
+			thisText = $(this).text().replace(/\s+/g, ''),
+			textLength = thisText.length;
+		if(constHeight >= targetHeight) return;
+		for (var count = 1; count <= textLength; count++) {
+			var justTextLength = textLength - count,
+				showText = thisText.substring(0, textLength - count),
+				hideText = thisText.substring(justTextLength, textLength),
+				insertText = showText;
+			insertText += '<span class="is-disable">' + hideText + '</span>';
+			insertText += '<span class="omit">…</span>';
+			$(this).html(insertText);
+			targetHeight = $(this).innerHeight();
+			if (constHeight >= targetHeight) {
+				break;
 			}
-		});
-		$(target + ' .hide').hide();
-	}
-}
+		}
+	});
+};
+
